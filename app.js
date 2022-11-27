62e03c25cbeaed6d4e53b00c3733d7f3f38dc167
Risad212 <hafez.risad@gmail.com>
2022-11-27 17:13:25 +0600

    'done'

---
+++ app.js
@@ -0,0 +1,15 @@
+/* =====================================================
+// 		toggle collapse button
+===================================================== */
+$(document).ready(function(){
+    $(".sabtn").click(function(){
+      $(".samajikota-box").slideToggle()
+    });
+});
+
+
+$(document).ready(function(){
+    $(".sabtn").hover(function(){
+      $(".samajikota-box").slideToggle()
+    });
+});
