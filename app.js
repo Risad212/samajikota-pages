ed35c13767f33dc839070e6764a6478175b300df
Risad212 <hafez.risad@gmail.com>
2022-12-05 18:30:24 +0600

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+let sabtn = document.querySelector('.sabtn')
+let sbox = document.querySelector('.collapse')
+let remove = document.querySelector('.removeicon');
+
+
+sabtn.addEventListener('click', () => {
+    sbox.style.display = 'block';
+    sbox.style.opacity = '1';
+    sbox.style.height = '550px';
+})
+
+
+
+
