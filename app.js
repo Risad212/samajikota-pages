0e8957695240c1de4c2dec1f17bcd6d96e7eb3ee
Mohammad Risad <83496132+Risad212@users.noreply.github.com>
2022-12-10 20:20:44 +0600

    'done'

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 // /* =====================================================
 // // 		toggle collapse button
 // ===================================================== */
-$(document).ready(function () {
-  $('.sabtn').click(function (e) {
-      $('#accoding').stop(true).slideToggle();
-  });
-  $(document).click(function (e) {
-      if (!$(e.target).closest('.sabtn, #accoding').length) {
-          $('#accoding').stop(true).slideUp();
-      }
-  });
-});
+// $(document).ready(function () {
+//   $('.sabtn').mouseon(function (e) {
+//       $('#accoding').stop(true).slideToggle();
+//   });
+// });
 
-// close icons box when click on remove Icon
-$(document).ready(function(){
-  $(".removeicon").click(function(){
-    $("#accoding").slideUp()
-  });
-});
+// $(document).click(function (e) {
+//   if (!$(e.target).closest('.sabtn, #accoding').length) {
+//       $('#accoding').stop(true).slideUp();
+//   }
+// });
+
+
+// // close icons box when click on remove Icon
+// $(document).ready(function(){
+//   $(".removeicon").click(function(){
+//     $("#accoding").slideUp()
+//   });
+// });
